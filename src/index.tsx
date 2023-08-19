e0eabccd0660e70a90c6674b72b8abc06b58a63f
Olga <kartoshkaslukom@gmail.com>
2023-08-19 13:26:16 +0200

    refactor: migrate routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './main.scss';
 import { createRoot } from 'react-dom/client';
+import { Routing } from './routing/routing';
 
 function NavigationBar() {
   return <h1>Hello from React!</h1>;
@@ -8,4 +9,4 @@ function NavigationBar() {
 
 const domNode = document.body;
 const root = createRoot(domNode);
-root.render(<NavigationBar />);
+root.render(<Routing />);
