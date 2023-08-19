2f5c42f47e3860c04c568074e1f0ab979b93e613
Olga <kartoshkaslukom@gmail.com>
2023-08-19 13:23:46 +0200

    refactor: migrate page 404

---
+++ src/modules/main/404/404-page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function NotFound() {
+  return (
+    <div>
+      <h1 className="404-title">404</h1>
+      <div className="404-text">Sorry, the page was not found</div>
+    </div>
+  );
+}
