48b795ee41787b9d2cfa9d99b6405afd9b2b0d0d
Olga <kartoshkaslukom@gmail.com>
2023-08-27 12:23:01 +0200

    feat: add interactivity

--- src/modules/main/catalog-product-page/catalogProductPage.tsx
+++ src/modules/main/catalog-product-page/catalogProductPage.tsx
@@ -52,6 +52,7 @@ export class CatalogPage extends React.Component<object, { products: ProductData
           };
         });
         this.setState({ products: elements });
+        console.log('successs');
       })
       .catch(() => {
         this.setState({ success: false });
@@ -62,17 +63,19 @@ export class CatalogPage extends React.Component<object, { products: ProductData
     return (
       <div className="catalog-page">
         <h1 className={this.state.success ? 'error-message' : 'visible'}>Sorry, cannot get data now</h1>
-        {this.state.products.map((product) => {
-          return (
-            <div className="catalog-item">
-              <div className="product-title">{product.name}</div>
-              <div>{product.description}</div>
-              <img src={product.image} alt="" className="catalog-img" />
-              <div className={product.discounted ? 'crossed' : ''}>{product.price}</div>
-              <div>{product.discount}</div>
-            </div>
-          );
-        })}
+        <div className="product-list">
+          {this.state.products.map((product) => {
+            return (
+              <div className="catalog-item">
+                <div className="product-title">{product.name}</div>
+                <div>{product.description}</div>
+                <img src={product.image} alt="" className="catalog-img" />
+                <div className={product.discounted ? 'crossed' : ''}>{product.price}</div>
+                <div>{product.discount}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
